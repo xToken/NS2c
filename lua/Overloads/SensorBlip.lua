93e37e08d84dd4960f7e73249cfe96f5430d52c0
Matt <mkreider@gmail.com>
2016-01-09 08:33:08 -0500

    hook for stupid modloader in ns2

--- lua/Overloads/SensorBlip.lua
+++ lua/Overloads/SensorBlip.lua
@@ -3,28 +3,21 @@
 // lua\Overloads\SensorBlip.lua
 // - Dragon
 
-Script.Load("lua/Class.lua")
+local origSensorBlipOnCreate = SensorBlip.OnCreate
+function SensorBlip:OnCreate()
+    origSensorBlipOnCreate(self)
+    self:UpdateRelevancy(0)
+end
 
-local origSensorBlipOnCreate
-origSensorBlipOnCreate = Class_ReplaceMethod("SensorBlip", "OnCreate", 
-	function(self)
-		origSensorBlipOnCreate(self)
-		self:UpdateRelevancy(0)
-	end
-)
-
-local origSensorBlipUpdateRelevancy
-origSensorBlipUpdateRelevancy = Class_ReplaceMethod("SensorBlip", "UpdateRelevancy", 
-	function(self, teamNum)
-		self:SetRelevancyDistance(Math.infinity)
-		local includeMask
-		if teamNum == 1 then
-			includeMask = kRelevantToTeam1
-		elseif teamNum == 2 then
-			includeMask = kRelevantToTeam2
-		else
-			includeMask = 0
-		end
-		self:SetExcludeRelevancyMask(includeMask)
-	end
-)
+function SensorBlip:UpdateRelevancy(teamNum)
+    self:SetRelevancyDistance(Math.infinity)
+    local includeMask
+    if teamNum == 1 then
+        includeMask = kRelevantToTeam1
+    elseif teamNum == 2 then
+        includeMask = kRelevantToTeam2
+    else
+        includeMask = 0
+    end
+    self:SetExcludeRelevancyMask(includeMask)
+end
